6491ab35911dde732a76418c143739213f5e5a74
Jack Dellamea <jdellamea@lcs.on.ca>
2023-01-27 14:17:40 -0500

    Re-organized the project

---
+++ FindTheRoots3/Views/ResultView.swift
@@ -0,0 +1,20 @@
+//
+//  ResultView.swift
+//  FindTheRoots3
+//
+//  Created by Jack Dellamea on 2023-01-27.
+//
+
+import SwiftUI
+
+struct ResultView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ResultView_Previews: PreviewProvider {
+    static var previews: some View {
+        ResultView()
+    }
+}
