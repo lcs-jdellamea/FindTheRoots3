deb6e6c1e7e8921ad9b25435fe73e85df592110b
Jack Dellamea <jdellamea@lcs.on.ca>
2023-01-27 14:21:08 -0500

    Added a new structure

---
+++ FindTheRoots3/Views/RootsCalculatorView.swift
@@ -0,0 +1,20 @@
+//
+//  RootsCalculatorView.swift
+//  FindTheRoots3
+//
+//  Created by Jack Dellamea on 2023-01-27.
+//
+
+import SwiftUI
+
+struct RootsCalculatorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RootsCalculatorView_Previews: PreviewProvider {
+    static var previews: some View {
+        RootsCalculatorView()
+    }
+}
