62d5240963d11ad2921fdb5927aec7b4dacc5471
Jack Dellamea <jdellamea@lcs.on.ca>
2023-01-27 14:25:39 -0500

    Finished copying project over

--- FindTheRoots3/Views/ResultView.swift
+++ FindTheRoots3/Views/ResultView.swift
@@ -2,19 +2,46 @@
 //  ResultView.swift
 //  FindTheRoots3
 //
-//  Created by Jack Dellamea on 2023-01-27.
+//  Created by Jack Dellamea on 2023-01-24.
 //
 
 import SwiftUI
 
 struct ResultView: View {
+    
+    //MARK: Computed properties
+    let somePriorResult: Result
+    
+    //MARK: Computed properties
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 10) {
+            
+            // Input values
+            HStack(spacing: 30) {
+                
+                
+                
+                Text("Hello")
+                
+                
+                
+                
+
+                
+            }
+            
+            // Output
+            Text(somePriorResult.roots)
+                .font(Font.custom("Times New Roman", size: 20.0,
+                                  relativeTo: .body))
+        }
+        .padding(.horizontal, 10)
+        .padding(.vertical, 15)
     }
 }
 
 struct ResultView_Previews: PreviewProvider {
     static var previews: some View {
-        ResultView()
+        ResultView(somePriorResult: resultForPreviews)
     }
 }
--- FindTheRoots3/Views/RootsCalculatorView.swift
+++ FindTheRoots3/Views/RootsCalculatorView.swift
@@ -2,19 +2,48 @@
 //  RootsCalculatorView.swift
 //  FindTheRoots3
 //
-//  Created by Jack Dellamea on 2023-01-27.
+//  Created by Jack Dellamea on 2023-01-24.
 //
 
 import SwiftUI
 
 struct RootsCalculatorView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    
+    //MARK: Stored properties
+    
+    // Input values for co-efficients
+    @State var a: Double = 1
+    @State var b: Double = -6
+    @State var c: Double = 8
+    
+    //List of prior results
+    @State var priorResults: [Result] = []
+    
+    //  MARK: Computed properties
+    
+    // The result, possibilities are:
+    // 1. Discriminant is negative, no real roots
+    // 2. Discriminant is zero, so two equal real roots
+    // 3. Discriminant is positive so two different real roots
+    var result: String {
+        
+        let discriminant = b * b - 4 * a * c
+        
+        //check for negative descriminant (meaning no solutions)
+        if discriminant < 0 {
+           return "no real roots."
+        } else {
+            let x1 = ( b * -1 - discriminant.squareRoot() ) / (2 * a)
+            let x2 = ( b * -1 + discriminant.squareRoot() ) / (2 * a)
+            
+            return "x ≈ \(x1.formatted(.number.precision(.fractionLength(2)))) and x ≈ \(x2.formatted(.number.precision(.fractionLength(2))))"
+        }
     }
-}
-
-struct RootsCalculatorView_Previews: PreviewProvider {
-    static var previews: some View {
-        RootsCalculatorView()
+    
+    // The usser interface
+    var body: some View {
+        VStack {}
+            .padding()
+            .navigationTitle("Find the Roots")
     }
 }
