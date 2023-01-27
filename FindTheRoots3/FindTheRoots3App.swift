19d808856750875ff790a99c05e6c69d5ceb35aa
Jack Dellamea <jdellamea@lcs.on.ca>
2023-01-27 14:16:02 -0500

    Initial Commit

---
+++ FindTheRoots3/FindTheRoots3App.swift
@@ -0,0 +1,17 @@
+//
+//  FindTheRoots3App.swift
+//  FindTheRoots3
+//
+//  Created by Jack Dellamea on 2023-01-27.
+//
+
+import SwiftUI
+
+@main
+struct FindTheRoots3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
